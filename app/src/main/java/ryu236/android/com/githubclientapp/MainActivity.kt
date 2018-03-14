161c7e2e74242b540529f8d33a89095af69c874f
Ryu236 <ryu.adv.2360@gmail.com>
2018-03-15 00:01:08 +0900

    change remote url

--- app/src/main/java/ryu236/android/com/githubclientapp/MainActivity.kt
+++ app/src/main/java/ryu236/android/com/githubclientapp/MainActivity.kt
@@ -1,29 +1,18 @@
 package ryu236.android.com.githubclientapp
 
-import android.app.AlertDialog
-import android.app.Dialog
-import android.app.DialogFragment
-import android.content.DialogInterface
 import android.content.Intent
 import android.content.SharedPreferences
-import android.net.Uri
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
 import android.preference.PreferenceManager
-import android.util.Log
 import android.widget.TextView
 import kotlinx.android.synthetic.main.activity_main.*
 import okhttp3.*
 import timber.log.Timber
-import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
-    val mClientId: String = "128f1acba0db81b630d7"
-    val mClientSecret: String = "00218364b4dccf7b70acd9b86f38520b6f6e1d41"
-    var mCode: String? = null
     var mAccessToken: String = ""
-    val client: OkHttpClient = OkHttpClient()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
